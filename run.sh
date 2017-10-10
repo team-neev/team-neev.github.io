1ab607eb5264e17768fb048ba634895e86fbfd18
Jakub Zalas <jakub@zalas.pl>
2017-10-10 09:10:49 +0100

    Add the stop command

--- run.sh
+++ run.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 set -e
+set -u
+set -o pipefail
 
-docker-compose up -d
+COMMAND=${1:-"help"}
 
-echo " ---- Browse to http://localhost:80 --- "
+function start() {
+    docker-compose up -d
+
+    echo " ---- Browse to http://localhost:80 --- "
+}
+
+function stop() {
+    docker-compose stop
+}
+
+function help() {
+    echo "Usage: $0 "$(compgen -A function | tr "\\n" "|" | sed 's/|$//')
+}
+
+if [ "$(type -t $COMMAND)" != "function" ]; then
+    help
+    exit 1
+fi
+
+$COMMAND ${@:2}
